4048e9dc1ae3572a74f1ca318e49cb229915633f
Dominic Waithe <dominic_waithe@hotmail.com>
2015-08-13 10:11:25 +0100

    For the msr file type, fixed the bug which means windows fails to recognise 'l' type in binary file import

--- FCS_scanning_correlator/simport_methods.py
+++ FCS_scanning_correlator/simport_methods.py
@@ -117,11 +117,27 @@ class Import_msr():
             len_of_name = struct.unpack('I', f.read(4))[0]
             #print 'name_of_len',len_of_name
             len_of_desc = struct.unpack('I', f.read(4))[0]
-            #print 'descr_len',len_of_desc
+            
+            #Windows does this a bit differently to other OS :-) always nice.
+            if platform.system() == 'Darwin':
 
-            reserved = struct.unpack('l', f.read(8))[0]
-            data_len_disk = struct.unpack('l', f.read(8))[0]
-            start_x = struct.unpack('l', f.read(8))[0]
+                    reserved =  struct.unpack('l', self.f.read(8))[0]
+                    data_len_disk = struct.unpack('l', f.read(8))[0]
+                    start_x = struct.unpack('l', f.read(8))[0]
+                elif platform.system() == 'Windows':
+                    reserved =  struct.unpack('l', self.f.read(4))[0]
+                    null =  struct.unpack('l', self.f.read(4))[0]
+                    data_len_disk =  struct.unpack('l', self.f.read(4))[0]
+                    null =  struct.unpack('l', self.f.read(4))[0]
+                    start_x =  struct.unpack('l', self.f.read(4))[0]
+                    null =  struct.unpack('l', self.f.read(4))[0]
+                else:
+                    reserved =  struct.unpack('l', self.f.read(8))[0]
+                    data_len_disk = struct.unpack('l', f.read(8))[0]
+                    start_x = struct.unpack('l', f.read(8))[0]
+
+
+            
             
             #print 'next_stack_pos',start_x
             if exit_state == True:
