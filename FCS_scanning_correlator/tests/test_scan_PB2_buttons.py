41fd22a526440170fce7164a47686330ceab7830
Dominic Waithe <dominic_waithe@hotmail.com>
2017-06-26 16:24:23 +0100

    added more photobleaching tests

--- FCS_scanning_correlator/tests/test_scan_PB2_buttons.py
+++ FCS_scanning_correlator/tests/test_scan_PB2_buttons.py
@@ -75,7 +75,8 @@ def test_scan_PB2_buttons(par_obj,win_obj):
 		assert win_obj.diag.isVisible() == False
 
 		
-		"""
+		#The following loads a collection of data which is too short etc... It shouldn't error, but you wouldn't use this data.
+		
 		QTest.mouseClick(win_obj.bleach_corr2_btn, QtCore.Qt.LeftButton)
 
 		assert win_obj.bleach_corr2_plugin.bleach_corr2_win.isVisible() == True
@@ -88,33 +89,35 @@ def test_scan_PB2_buttons(par_obj,win_obj):
 		QTest.mouseClick(win_obj.bleach_corr2_plugin.apply_to_all_data_btn, QtCore.Qt.LeftButton)
 
 
+		#The last data file should work.
+		assert win_obj.bleach_corr_on_off.text() == 'M2 ON '
+
+		for i in range(4,-1,-1):
+			QTest.mouseClick(win_obj.xb[i], QtCore.Qt.LeftButton)
+		assert par_obj.numOfLoaded == 0
 		
-		assert win_obj.bleach_corr_on_off.text() == '  OFF  '
-		QTest.mouseClick(win_obj.bleach_corr1_plugin.apply_corr_btn, QtCore.Qt.LeftButton)
-		QTest.mouseClick(win_obj.bleach_corr1_plugin.export_trace_btn, QtCore.Qt.LeftButton)
-		assert win_obj.bleach_corr_on_off.text() == 'M1 ON '
-
-		print 'self.', np.sum(win_obj.bleach_corr1_plugin.objId.CH0_pc)
-
-
-
-
-		win_obj.bleach_corr1_plugin.close()
-		assert win_obj.bleach_corr1_plugin.bleach_corr1_win.isVisible() == False
-
 		
+
+		###Now we open a file and repeat the Photobleaching analysis on it several times. Used to produce an error.
+
+
 
 
 
 		win_obj.test_path = [os.getcwd()+'/test_files/out_img_D_0p2_noi_0_drift_False_ts_30000_mol_120_bleach_False_prob_0_numCH2_with_PBC.tif']
-		win_obj.test_path[0] = win_obj.test_path[0].replace('\\','/')
+
+
+		for i in range(0, win_obj.test_path.__len__()):
+			win_obj.test_path[i] = win_obj.test_path[i].replace('\\','/')
 
 		QTest.mouseClick(win_obj.openFile, QtCore.Qt.LeftButton)
 		win_obj.file_import.file_dialog.done(1,win_obj.test_path)
 
+				
+
 		#Open the dialog for the line sampling (Hz):
-		assert win_obj.diag.line_sampling_win.isVisible() == True
-		assert win_obj.diag.input_text.isVisible() == True
+		assert win_obj.diag.line_sampling_win.isVisible() == True, "Dialog for line sampling did not open."
+		assert win_obj.diag.input_text.isVisible() == True, "Text input for line sampling did not open."
 		assert str(win_obj.diag.label.text()) == 'Enter the line sampling (Hz):'
 		QTest.keyClicks(win_obj.diag.input_text, str(1800))
 		assert win_obj.diag.input_text.text() == str(1800)
@@ -129,51 +132,51 @@ def test_scan_PB2_buttons(par_obj,win_obj):
 		QTest.mouseClick(win_obj.diag.ok, QtCore.Qt.LeftButton)
 		assert win_obj.diag.dialog_dwell_win.isVisible() == False
 
-		#assert win_obj.diag.use_settings_win.isVisible() == False
-		#QTest.mouseClick(win_obj.diag.no, QtCore.Qt.LeftButton)
-		#assert win_obj.diag.use_settings_win.isVisible() == False
+		temp = np.copy(win_obj.carpet_img)
 
-		#Check that the line sampling dialog doesn't appear again.
-		assert win_obj.diag.line_sampling_win.isVisible() == False
+		QTest.mouseClick(win_obj.bleach_corr2_btn, QtCore.Qt.LeftButton)
+		assert win_obj.bleach_corr2_plugin.bleach_corr2_win.isVisible() == True
+		QTest.mouseClick(win_obj.bleach_corr2_plugin.preview_selection_btn, QtCore.Qt.LeftButton)
+		win_obj.bleach_corr2_plugin.duration_combo.setCurrentIndex(3)
+		QTest.mouseClick(win_obj.bleach_corr2_plugin.export_trace_btn, QtCore.Qt.LeftButton)
+		win_obj.bleach_corr2_plugin.close()
+		
+		#Switch on and off the correction.
 
-		#Check that the previous and next pane visualisation works well.
-		QTest.mouseClick(win_obj.prev_pane, QtCore.Qt.LeftButton)
-		for objId in par_obj.objectRef:
-			if(objId.cb.isChecked() == True):
-				assert objId.pane == 0
-				break;
-		
-		
-		QTest.mouseClick(win_obj.bleach_corr1_btn, QtCore.Qt.LeftButton)
-		assert win_obj.bleach_corr1_plugin.bleach_corr1_win.isVisible() == True
-		
-		assert win_obj.bleach_corr_on_off.text() == '  OFF  '
-		assert np.sum(win_obj.bleach_corr1_plugin.objId.CH0_pc) == 0 
-		assert np.sum(win_obj.bleach_corr1_plugin.objId.CH1_pc) == 0
-		QTest.mouseClick(win_obj.bleach_corr1_plugin.apply_corr_btn, QtCore.Qt.LeftButton)
-		QTest.mouseClick(win_obj.bleach_corr1_plugin.export_trace_btn, QtCore.Qt.LeftButton)
-		assert win_obj.bleach_corr_on_off.text() == 'M1 ON '
+		temp2 = np.copy(win_obj.carpet_img)
+		assert np.sum(temp2) != np.sum(temp), "Your carpet didn't change subsequent to applying photobleaching."
+		QTest.mouseClick(win_obj.bleach_corr_on_off, QtCore.Qt.LeftButton)
+		temp3 = np.copy(win_obj.carpet_img)
+		assert np.sum(temp3) == np.sum(temp), "Your carpet hasn't returned to normal by turning off the correction"
+		QTest.mouseClick(win_obj.bleach_corr_on_off, QtCore.Qt.LeftButton)
+		assert np.sum(temp2) != np.sum(temp), "Your carpet didn't change subsequent to applying photobleaching correction again."
 
-		
-		#Check the carpet is modified by the photobleaching.
+		QTest.mouseClick(win_obj.bleach_corr2_btn, QtCore.Qt.LeftButton)
+		assert win_obj.bleach_corr2_plugin.bleach_corr2_win.isVisible() == True
+		QTest.mouseClick(win_obj.bleach_corr2_plugin.preview_selection_btn, QtCore.Qt.LeftButton)
+		#win_obj.bleach_corr2_plugin.duration_combo.setCurrentIndex(3)
+		QTest.mouseClick(win_obj.bleach_corr2_plugin.export_trace_btn, QtCore.Qt.LeftButton)
+		win_obj.bleach_corr2_plugin.close()
 
-		assert np.sum(win_obj.bleach_corr1_plugin.objId.CH0_pc) != 0 
-		assert np.sum(win_obj.bleach_corr1_plugin.objId.CH1_pc) != 0
-		assert np.sum(win_obj.bleach_corr1_plugin.objId.CH0_pc) != np.sum(win_obj.bleach_corr1_plugin.objId.CH0)
-		assert np.sum(win_obj.bleach_corr1_plugin.objId.CH1_pc) != np.sum(win_obj.bleach_corr1_plugin.objId.CH1)
-		assert np.sum(win_obj.bleach_corr1_plugin.objId.CH1_pc) != np.sum(win_obj.bleach_corr1_plugin.objId.CH0_pc)
+		temp2 = np.copy(win_obj.carpet_img)
+		assert np.sum(temp2) != np.sum(temp), "Your carpet didn't change subsequent to applying photobleaching."
+		QTest.mouseClick(win_obj.bleach_corr_on_off, QtCore.Qt.LeftButton)
+		temp3 = np.copy(win_obj.carpet_img)
+		assert np.sum(temp3) == np.sum(temp), "Your carpet hasn't returned to normal by turning off the correction"
+		QTest.mouseClick(win_obj.bleach_corr_on_off, QtCore.Qt.LeftButton)
+		assert np.sum(temp2) != np.sum(temp), "Your carpet didn't change subsequent to applying photobleaching correction again."
 
-		win_obj.bleach_corr1_plugin.close()
-		assert win_obj.bleach_corr1_plugin.bleach_corr1_win.isVisible() == False
-		
-		assert par_obj.numOfLoaded == 2
-		for i in range(1,-1,-1):
+
+
+		##Now we can export the photobleached carpet and the unphotobleached carpet.
+
+		QTest.mouseClick(win_obj.export_region_btn, QtCore.Qt.LeftButton)
+		QTest.mouseClick(win_obj.bleach_corr_on_off, QtCore.Qt.LeftButton)
+		QTest.mouseClick(win_obj.export_region_btn, QtCore.Qt.LeftButton)
+
+
+		for i in range(0,-1,-1):
 			QTest.mouseClick(win_obj.xb[i], QtCore.Qt.LeftButton)
 		assert par_obj.numOfLoaded == 0
-		
-		
 
-		
-		
-		"""
 
